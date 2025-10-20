915e82b85099202640349a15b80495d26238c7aa
Michael Levy <michaelfeehanlevy@gmail.com>
2025-10-20 19:45:22 +1300

    fixes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,8 @@
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
-  // Find and copy any `jpg` files, maintaining directory structure.
   eleventyConfig.addPassthroughCopy("**/*.css");
-  eleventyConfig.addPassthroughCopy("**/*.js");
+  eleventyConfig.addPassthroughCopy("**/favicon.png");
   eleventyConfig.addPassthroughCopy("**/*.ttf");
   eleventyConfig.addPassthroughCopy("**/*.otf");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
