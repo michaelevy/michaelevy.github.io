05877dd8275cbc34ffed31f3922f47ae1e992954
Michael Levy <michaelfeehanlevy@gmail.com>
2025-10-20 19:55:43 +1300

    attempt to fix cname

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("**/*.css");
   eleventyConfig.addPassthroughCopy("**/favicon.png");
+  eleventyConfig.addPassthroughCopy("**/*.js");
   eleventyConfig.addPassthroughCopy("**/*.ttf");
   eleventyConfig.addPassthroughCopy("**/*.otf");
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
