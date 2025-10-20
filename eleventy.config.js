23afef7cdcbc4c4925a6201bd449fc9463d92eae
Michael Levy <michaelfeehanlevy@gmail.com>
2025-10-20 19:40:11 +1300

    eleventy image rendering

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,10 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function (eleventyConfig) {
-	// Find and copy any `jpg` files, maintaining directory structure.
-	eleventyConfig.addPassthroughCopy("**/*.css");
-	eleventyConfig.addPassthroughCopy("**/*.js");
-	eleventyConfig.addPassthroughCopy("resources");
+  // Find and copy any `jpg` files, maintaining directory structure.
+  eleventyConfig.addPassthroughCopy("**/*.css");
+  eleventyConfig.addPassthroughCopy("**/*.js");
+  eleventyConfig.addPassthroughCopy("**/*.ttf");
+  eleventyConfig.addPassthroughCopy("**/*.otf");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 }
