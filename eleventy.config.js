cc8a164db1d63f077bcaeb0b2eded42baa2f163e
michaelevy <michaelevy@users.noreply.github.com>
2025-10-20 06:46:53 +0000

    deploy: a38f4e6bc72ccc6dd0507fec94a00f8b368308e4

--- eleventy.config.js
+++
@@ -1,6 +0,0 @@
-export default function (eleventyConfig) {
-	// Find and copy any `jpg` files, maintaining directory structure.
-	eleventyConfig.addPassthroughCopy("**/*.css");
-	eleventyConfig.addPassthroughCopy("**/*.js");
-	eleventyConfig.addPassthroughCopy("resources");
-}
